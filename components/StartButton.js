84e22f7c9f647fe567df6fc4ec3280733cc9c4d8
Viviane <arrghe.dev@gmail.com>
2024-06-11 10:32:31 +0200

    suppression de fichiers non utilisés & changement du bouton de déparrage

--- components/StartButton.js
+++ components/StartButton.js
@@ -11,10 +11,10 @@ const StartButton = () => {
   };
 
   return (
-    <div className="flex flex-col bg-slate-950  h-20 w-20 rounded ">
+    <div className="flex flex-col bg-slate-950  h-20 w-20">
       <div
-        style={{ "background-image": "url('/tetrisbtn.png')" }}
-        className=" bg-contain h-20 rounded border-white border-2 cursor-pointer bg-no-repeat bg-center"
+        style={{ "background-image": "url('/btnTetris1.png')" }}
+        className=" bg-contain h-20 rounded-2xl border-white border-2 cursor-pointer bg-no-repeat bg-center"
         onClick={handleStart}
       >
       </div>
